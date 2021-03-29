2e319750d6641a94562e467ca8f749c869866396
Justin Peczenij <justinpeczenij@gmail.com>
2021-03-29 13:15:28 -0400

    installed styled-components, created Login and SignUp components under Unit_2, layed out forms and basic styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,61 @@
+import Login from './Unit_2/Login'
+import SignUp from './Unit_2/SignUp'
 import './App.css';
+import { Route, Link, Switch } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+  display:flex;
+  justify-content: space-around;
+  align-items:center;
+`;
+
+const StyledH2 = styled.h2`
+  color: greenyellow;
+  font-size:1.8rem;
+  font-style:italic;
+`;
+
+const StyledNav = styled.nav`
+  width: 50%;
+  text-align:end;
+`;
+
+const StyledNavButton = styled.button`
+  width: 10%;
+  margin: 4% 1%;
+  padding:1% 0;
+  border-radius: 15%;
+  border: none;
+`;
 
 function App() {
-	return (
-		<div className='App'>
-			<h1>Fresh App</h1>
-		</div>
-	);
-}
+  return (
+    <div className='app'>
+        <StyledHeader>
+          <StyledH2>Water My Plants!</StyledH2>
+          <StyledNav>
+            <Link to='/login'>
+              <StyledNavButton style={{color:'white', backgroundColor:'greenyellow'}}>Login</StyledNavButton>
+            </Link>  
+            <Link to='/signup'>
+                <StyledNavButton>Sign Up</StyledNavButton>
+            </Link>
+          </StyledNav>
+        </StyledHeader>
+        <Switch>
+          <Route path='/signup'>
+			  <SignUp />
+          </Route>
+          <Route path='/login'>
+			  <Login />
+          </Route>
+          <Route exact path='/'>
+          </Route>
+        </Switch>
+
+    </div>
+  );
+};
 
 export default App;
---
+++ frontend/src/Unit_2/Login.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components'
+
+const StyledDiv = styled.div`
+    margin: auto;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+`;
+
+const StyledForm = styled.form`
+    margin: auto;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+`;
+
+const StyledInput = styled.input`
+    margin-bottom: 2%;
+`;
+
+export default function Login(props) {
+    return (
+        <StyledDiv>
+            <h1>Login</h1>
+            <StyledForm>
+                <StyledInput 
+                    name=''
+                    type=''
+                    value=''
+                    onChange=''
+                    placeholder='Username'
+                />
+                <StyledInput 
+                    name=''
+                    type=''
+                    value=''
+                    onChange=''
+                    placeholder='Password'
+                />
+            </StyledForm>
+        </StyledDiv>
+    )
+}
