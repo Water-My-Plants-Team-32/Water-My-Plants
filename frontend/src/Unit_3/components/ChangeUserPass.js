5796d25617ef9eaedb4d4f14565f62cf6286dac7
eet349 <51262590+eet349@users.noreply.github.com>
2021-04-01 21:06:04 -0500

    fixes a bug with changePassword.

--- frontend/src/Unit_3/components/ChangeUserPass.js
+++ frontend/src/Unit_3/components/ChangeUserPass.js
@@ -1,46 +1,51 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-const ChangeUserPass = ({user, setChanging, changeUserPass}) => {
-    const initialPasswordState = {
-        password: user.password
-    }
+const ChangeUserPass = ({ user, setChanging, changeUserPass, setSuccess }) => {
+	const initialPasswordState = {
+		// password: user.password,
+		password: '',
+	};
 
-    const [password, setPassword] = useState(initialPasswordState);
-    
-    const handleChange = (event) =>{
-        const value = event.target.value;
+	const [password, setPassword] = useState(initialPasswordState);
+
+	useEffect(() => {
+		console.log('password: ', password);
+	});
+
+	const handleChange = (event) => {
+		const value = event.target.value;
 		setPassword({ ...password, [event.target.name]: value });
-    }
-    
-    const handleChangePassword = (event) => {
-        event.preventDefault();
-        console.log('userid: ', user.userid)
-        axiosWithAuth()
-             .put(`/api/users/user/${user.userid}`, password)
-             .then((response) => {
-                 console.log(password);
-                 changeUserPass(response.data.password);
-                 setChanging(false);
-             })
-             .catch((error) => {
-                 console.error(error);
-             })
-    }
+	};
 
-    return (
-        <>
-            <form onSubmit={handleChangePassword}>
-                <input
-                    name='phonenumber'
-                    type='password'
-                    value={password.password}
-                    onChange={handleChange}
-                />
-                <button>Change</button>
-            </form>
-        </>
-    )
+	const handleChangePassword = (event) => {
+		event.preventDefault();
+		console.log('userid: ', user.userid);
+		axiosWithAuth()
+			.put(`/api/users/user/${user.userid}`, password)
+			.then((response) => {
+				console.log(password);
+				changeUserPass(response.data.password);
+				setChanging(false);
+				setSuccess('Password updated.');
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	};
 
-}
+	return (
+		<>
+			<form onSubmit={handleChangePassword}>
+				<input
+					name='password'
+					type='password'
+					value={password.password}
+					onChange={handleChange}
+				/>
+				<button>Change</button>
+			</form>
+		</>
+	);
+};
 export default ChangeUserPass;
